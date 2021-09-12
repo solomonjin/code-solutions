a14afe503224fe01f666fb593482d430f1bd7267
Solomon Jin <solomonjin135@yahoo.com>
2021-09-12 04:00:42 +0000

    Optimized solution for remove duplicates from sorted array.

--- leetcode/remove-duplicates-sorted.js
+++ leetcode/remove-duplicates-sorted.js
@@ -1,12 +1,22 @@
 var removeDuplicates = function (nums) {
-  const hash = {};
-  for (let i = 0; i < nums.length; i++) {
-    if (hash[nums[i]]) {
-      nums.splice(i, 1);
-      i--;
-    } else {
-      hash[nums[i]] = 1;
+  let index = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[index++] = nums[i]
     }
   }
-  return nums.length;
+  return index;
 };
+
+// var removeDuplicates = function (nums) {
+//   const hash = {};
+//   for (let i = 0; i < nums.length; i++) {
+//     if (hash[nums[i]]) {
+//       nums.splice(i, 1);
+//       i--;
+//     } else {
+//       hash[nums[i]] = 1;
+//     }
+//   }
+//   return nums.length;
+// };
