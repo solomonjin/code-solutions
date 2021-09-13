36137c72bd357fd671997cc3efcfccf2d381f365
Solomon Jin <solomonjin135@yahoo.com>
2021-09-13 14:51:00 -0700

    Implemented second solution to problem

--- leetcode/length-of-last-word.js
+++ leetcode/length-of-last-word.js
@@ -1,5 +1,12 @@
+//Second solution; Implemented the trim method (removes whitespace at beginning and end of string)
 var lengthOfLastWord = function (s) {
-  const words = s.split(' ');
-  const filtered = words.filter(word => word.length !== 0)
-  return filtered[filtered.length - 1].length;
+  const words = s.trim().split(' ');
+  return words[words.length - 1].length;
 };
+
+// First solution
+// var lengthOfLastWord = function (s) {
+//   const words = s.split(' ');
+//   const filtered = words.filter(word => word.length !== 0)
+//   return filtered[filtered.length - 1].length;
+// };
