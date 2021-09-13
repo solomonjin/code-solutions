68c55465b1ef8c590fb57529fbe7b5596b4e3bda
Solomon Jin <solomonjin135@yahoo.com>
2021-09-13 15:01:36 -0700

    Added a more optimized solution to the problem.

--- leetcode/length-of-last-word.js
+++ leetcode/length-of-last-word.js
@@ -1,3 +1,17 @@
+var lengthOfLastWord = function (s) {
+  const words = s.split(' ');
+  let index = words.length - 1;
+  while (true) {
+    if (words[index] === '') {
+      words.pop();
+      index--;
+    } else {
+      break;
+    }
+  }
+  return words[index].length
+};
+
 //Second solution; Implemented the trim method (removes whitespace at beginning and end of string)
 var lengthOfLastWord = function (s) {
   const words = s.trim().split(' ');
