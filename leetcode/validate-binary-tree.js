61a7d54602b5524b3be7abb100e3fed476f11fd4
Solomon Jin <solomonjin135@yahoo.com>
2023-11-04 13:59:22 -0700

    Nov 4 2023 - Code Solutions - validate binary tree
    - kth smallest element in bst

---
+++ leetcode/validate-binary-tree.js
@@ -0,0 +1,8 @@
+var isValidBST = function (root) {
+  const dfs = (node, min, max) => {
+    if (!node) return true;
+    if (node.val <= min || node.val >= max) return false;
+    return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
+  }
+  return dfs(root, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+};
