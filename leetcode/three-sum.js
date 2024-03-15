2d2bc3f55574afebaff58c89a38d6c557a269d74
Solomon Jin <solomonjin135@yahoo.com>
2024-03-15 14:26:06 -0700

    March 15 2024 - Code Solutions - review three sum
    - review container with most water

--- leetcode/three-sum.js
+++ leetcode/three-sum.js
@@ -11,6 +11,29 @@ If the next a value is the same as the previous one, skip it
 
 var threeSum = function (nums) {
   const result = [];
+  nums.sort((a, b) => a - b);         // Sort array
+
+  for (let i = 0; i < nums.length; i++) {     // Go through the array and for each number, do a 2 sum with l, r pointers with the remaining numbers in the array
+    if (i > 0 && nums[i] === nums[i - 1]) continue;     // Skip any repeated numbers
+
+    let [l, r] = [i + 1, nums.length - 1];
+
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r];
+      if (sum > 0) r--;
+      else if (sum < 0) l++;
+      else {
+        result.push([nums[i], nums[l], nums[r]]);
+        l++;
+        while (nums[l] === nums[l - 1] && l < r) l++;   // Skip any repeated numbers on the left pointer to avoid duplicate triplets
+      }
+    }
+  }
+  return result;
+};
+
+var threeSum = function (nums) {
+  const result = [];
   nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length; i++) {
