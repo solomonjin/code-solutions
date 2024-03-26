5ebfe98ddf72cf539103f063f500bd325cb85eda
Solomon Jin <solomonjin135@yahoo.com>
2024-03-26 15:06:54 -0700

    March 26 2024 - Code Solutions - review best time to buy and sell stock
    - review substring without repeating char

--- leetcode/best-time-to-buy-and-sell-stock.js
+++ leetcode/best-time-to-buy-and-sell-stock.js
@@ -1,17 +1,19 @@
 /*
-    Sliding window
+Sliding window
+- Set two variables (l, r) at the first and second indices of the input
+- Go through the array using the right pointer
+- For each step, check to see if the profit (right - left) is biggest before incrementing
+- But, if the number at right pointer is smaller than left, move left pointer to right's position before incrementing right
+- This will automatically set the left pointer to the lowest price point in the input eventually
 */
 
 var maxProfit = function (prices) {
-  if (prices.length <= 1) return 0;
   let [l, r, profit] = [0, 1, 0];
 
   while (r < prices.length) {
-    if (prices[r] <= prices[l]) {
-      l = r;
-    }
-    profit = Math.max(profit, prices[r] - prices[l]);
-    r++;
+    if (profit[l] > profit[r]) l = r;                       // Set left pointer to right, if left number is bigger
+    profit = Math.max(profit, prices[r] - prices[l]);       // Check to see if current profit is biggest
+    r++;                                                    // Increment right pointer to check next sequence
   }
   return profit;
 };
