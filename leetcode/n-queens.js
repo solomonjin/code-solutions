a4bcaaef9d259eb1dd01caed6abd58e12e39f989
Solomon Jin <solomonjin135@yahoo.com>
2024-04-05 15:11:52 -0700

    April 5 2024 - Code Solutions - review n queens

--- leetcode/n-queens.js
+++ leetcode/n-queens.js
@@ -1,32 +1,47 @@
+/*
+Backtrack/DFS:
+- To keep track of which spaces a queen is allowed in, create 3 sets:
+    - cols set
+    - positive diagonal set
+    - negative diagonal set
+- The rows will be accounted for by incrementing based on the row (Only one queen per row)
+- The diagonals are calculated by "slope". For pos diag (r + c), neg diag (r - c);
+- Create an empty board n x n
+- backtrack function:
+    - base case: num queens = n OR the row = n since we're placing one queen per row
+    - otherwise, go through each col space within the row and try placing the queen there IF it's valid
+    - mark the spots as having a queen within the appropriate sets, and mark the tile as a
+    - then recursively check the next row (Decision to add queen at that spot)
+    - Afterwards, remove the queen from that spot and check the next (remove the marked col/diags from sets)
+*/
 var solveNQueens = function (n) {
-  const col = new Set();
-  const posDiag = new Set(); // r + c
-  const negDiag = new Set(); // r - c
+  const cols = new Set();
+  const posDiag = new Set();  // r + c slope
+  const negDiag = new Set();  // r - c slope
 
+  const board = new Array(n).fill().map(() => new Array(n).fill('.')); // Create a n x n board filled with '.' tiles
   const res = [];
-  const board = new Array(n).fill().map(() => new Array(n).fill('.'));
 
   const backtrack = (r) => {
-    if (r === n) {
-      const copy = board.map(row => row.join(''));
-      res.push(copy);
+    if (r === n) {                                      // Possible solution found
+      res.push(board.map(r => r.join('')));         // Create a copy of the board
       return;
     }
 
     for (let c = 0; c < n; c++) {
-      if (col.has(c) || posDiag.has(r + c) || negDiag.has(r - c)) continue;
+      if (cols.has(c) || posDiag.has(r + c) || negDiag.has(r - c)) continue;      // Check if queen can be placed on current tile
 
-      col.add(c);
-      posDiag.add(r + c);
+      cols.add(c);                            // Place queen on current tile
+      posDiag.add(r + c);                     // Mark the diagonals and cols the queen occupies
       negDiag.add(r - c);
-      board[r][c] = 'Q';
+      board[r][c] = 'Q';                      // Mark tile as having a queen
 
-      backtrack(r + 1);
+      backtrack(r + 1);                       // Continue to find other queen placements if the queen is placed on current tile
 
-      col.delete(c);
-      posDiag.delete(r + c);
+      cols.delete(c);
+      posDiag.delete(r + c);                     // Decision to not place the queen on this tile (remove marked diags/cols and tile)
       negDiag.delete(r - c);
-      board[r][c] = '.';
+      board[r][c] = '.'                          // Reset tile to original state
     }
   }
 
