cbe4143930dd8194aef5d98a39c66e8b99c39e93
Solomon Jin <solomonjin135@yahoo.com>
2024-04-09 13:28:41 -0700

    April 9 2024 - Code Solutions - review max area of island

--- leetcode/max-area-of-islands.js
+++ leetcode/max-area-of-islands.js
@@ -1,19 +1,32 @@
+/*
+DFS Approach:
+- Similar to find number of islands in that we just need to find each island, but this time also get the area of it
+- Can recursively count the area of the island by just adding up the recursive calls of every adjacent tile then adding 1 to it
+- base case of DFS: if tile is out of bounds or not a land, return 0
+- Otherwise, set the current land tile to water and then return the area by recursively finding
+other land tiles and returning the sum of those calls + 1
+- Then, just need to go through the input grid and apply the dfs search to every tile, keeping track of the biggest
+area returned by the islands
+*/
 var maxAreaOfIsland = function (grid) {
-  if (grid.length <= 0 || grid[0].length <= 0) return 0;
-  let maxArea = 0;
   const [rows, cols] = [grid.length, grid[0].length];
-
   const dfs = (r, c) => {
-    if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 0) return 0;
-    grid[r][c] = 0;
-    return dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1) + 1;
+    if (r >= rows || r < 0 ||               // Base case: if tile out of bounds or tile is water
+      c >= cols || c < 0 ||
+      !grid[r][c]) {
+      return 0;
+    }
+
+    grid[r][c] = 0;                           // Current tile is land, so swap it to water (avoid revisiting tile)
+    return dfs(r + 1, c) + dfs(r - 1, c) +
+      dfs(r, c + 1) + dfs(r, c - 1) + 1;  // Return sum of lands from adjacent tiles plus itself (+1);
   }
 
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < cols; c++) {
-      if (grid[r][c] === 1) maxArea = Math.max(maxArea, dfs(r, c));
+  let maxArea = 0;
+  for (let r = 0; r < rows; r++) {                                    // Go through the input grid looking for land tiles
+    for (let c = 0; c < cols; c++) {                                // and check to see if each island returns the biggest island
+      if (grid[r][c]) maxArea = Math.max(maxArea, dfs(r, c));
     }
   }
-
   return maxArea;
 };
