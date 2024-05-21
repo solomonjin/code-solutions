917c60b533ed45061b778f3fdb6906188e6d8470
Solomon Jin <solomonjin135@yahoo.com>
2024-05-20 20:48:40 -0700

    May 20 2024 - Code Solutions - surrounded regions review

--- leetcode/surrounded-regions.js
+++ leetcode/surrounded-regions.js
@@ -1,33 +1,51 @@
-var solve = function (board) {
-  const [rows, cols] = [board.length, board[0].length];
+class Solution {
+  /*
+  DFS Approach:
+  - Think about the problem backwards. Instead of trying to find the tiles that
+  need to be flipped (O > X), find the O tiles that are NOT surrounded and can't be
+  flipped (O > O)
+  - The O tiles that are not surrounded are the Os along the borders as well as any
+  adjacently connected Os to them
+  - Create a DFS function to mark the tiles that are not surrounded
+      - Base case: Out of bounds or tile is not an O
+      - For every border O tile, mark it as an unsurrounded tile (T) then check its adjacent neighbors
+      for more unsurrounded Os
+  - Starting from the borders, use the DFS function to find and mark all unsurrounded Os
+  - Then, once all unsurrounded Os are marked (O > T), go through the grid
+  and flip any remaining Os (unmarked Os = surrounded, O > X) then revert the marked Os (T > O)
+  */
+  solve(board) {
+    const [rows, cols] = [board.length, board[0].length];
 
-  const capture = (r, c) => {
-    // Base Case: Out of bounds or value is not an O
-    if (r < 0 || c < 0 || r >= rows || c >= cols || board[r][c] !== 'O') return;
-    board[r][c] = 'T';
-    capture(r + 1, c);
-    capture(r - 1, c);
-    capture(r, c + 1);
-    capture(r, c - 1);
-  }
+    // DFS to help mark unsurrounded O tiles
+    const findUnsurrounded = (r, c) => {
+      // Base case: Tile out of bounds or not an O
+      if (r >= rows || r < 0 || c >= cols || c < 0 || board[r][c] !== 'O') return;
+      board[r][c] = 'T';  // Mark tile as unsurrounded
+      findUnsurrounded(r + 1, c);     // Then check adjacent tiles for connected Os
+      findUnsurrounded(r - 1, c);
+      findUnsurrounded(r, c + 1);
+      findUnsurrounded(r, c - 1);
+    }
 
-  // 1) Mark un-surrounded regions (O -> T by DFS)
-  // Left and right borders
-  for (let r = 0; r < rows; r++) {
-    capture(r, 0);
-    capture(r, cols - 1);
-  }
-  // Top and bottom borders
-  for (let c = 0; c < cols; c++) {
-    capture(0, c);
-    capture(rows - 1, c);
-  }
-
-  // 2) Capture surrounded regions (O -> X, T -> O)
-  for (let r = 0; r < rows; r++) {
+    // Check borders for O tiles (unsurrounded)
+    // left and right borders
+    for (let r = 0; r < rows; r++) {
+      findUnsurrounded(r, 0);
+      findUnsurrounded(r, cols - 1);
+    }
+    // Top and bottom borders
     for (let c = 0; c < cols; c++) {
-      if (board[r][c] === 'O') board[r][c] = 'X';
-      else if (board[r][c] === 'T') board[r][c] = 'O';
+      findUnsurrounded(0, c);
+      findUnsurrounded(rows - 1, c);
+    }
+
+    // Go through board and flip any surrounded tiles (Os) and revert marked tiles (T)
+    for (let r = 0; r < rows; r++) {
+      for (let c = 0; c < cols; c++) {
+        if (board[r][c] === 'O') board[r][c] = 'X';
+        else if (board[r][c] === 'T') board[r][c] = 'O';
+      }
     }
   }
-};
+}
